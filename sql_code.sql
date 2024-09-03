486182ae73c56c1c78d163a5ef1ab9dc4d25eed8
SowmyaKodityala <sowmyaks968@gmail.com>
2024-09-02 18:46:18 -0700

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -59,22 +59,4 @@ where rn=1
 
 
 
---which sub category had highest growth by profit in 2023 compare to 2022
-with cte as (
-select sub_category,year(order_date) as order_year,
-sum(sale_price) as sales
-from df_orders
-group by sub_category,year(order_date)
---order by year(order_date),month(order_date)
-	)
-, cte2 as (
-select sub_category
-, sum(case when order_year=2022 then sales else 0 end) as sales_2022
-, sum(case when order_year=2023 then sales else 0 end) as sales_2023
-from cte 
-group by sub_category
-)
-select top 1 *
-,(sales_2023-sales_2022)
-from  cte2
-order by (sales_2023-sales_2022) desc
+
